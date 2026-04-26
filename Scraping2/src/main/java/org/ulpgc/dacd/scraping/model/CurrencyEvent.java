6017f6e1804b6081109c4b8437ccf61f0eabde45
gaelsanchez18 <gaelsanchezgonzalez18@gmail.com>
2026-04-26 13:10:45 +0100

    Refactor Scraping2: reemplazo NewsEvent por CurrencyEvent

---
+++ Scraping2/src/main/java/org/ulpgc/dacd/scraping/model/CurrencyEvent.java
@@ -0,0 +1,19 @@
+package org.ulpgc.dacd.scraping.model;
+
+import java.time.Instant;
+
+public class CurrencyEvent {
+    private final String ts;
+    private final String ss;
+    private final Object data;
+
+    public CurrencyEvent(Object data, String source) {
+        this.ts = Instant.now().toString();
+        this.ss = source;
+        this.data = data;
+    }
+
+    public String getTs() { return ts; }
+    public String getSs() { return ss; }
+    public Object getData() { return data; }
+}
