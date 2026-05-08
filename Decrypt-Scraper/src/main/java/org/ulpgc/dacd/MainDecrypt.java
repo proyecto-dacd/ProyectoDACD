73e2f5576c449c18b93c974e95c7b7533239e2a6
gaelsanchez18 <gaelsanchezgonzalez18@gmail.com>
2026-05-08 13:09:15 +0100

    Renombrar modulo del scraper

---
+++ Decrypt-Scraper/src/main/java/org/ulpgc/dacd/MainDecrypt.java
@@ -0,0 +1,26 @@
+    package org.ulpgc.dacd;
+
+    import org.ulpgc.dacd.controller.NewsController;
+    import org.ulpgc.dacd.infrastructure.scraper.DecryptNewsFeeder;
+    import org.ulpgc.dacd.infrastructure.scraper.NewsFeeder;
+
+    import java.util.concurrent.Executors;
+    import java.util.concurrent.ScheduledExecutorService;
+    import java.util.concurrent.TimeUnit;
+
+    public class MainDecrypt {
+        public static void main(String[] args) {
+
+            NewsFeeder feeder = new DecryptNewsFeeder();
+            NewsController controller = new NewsController(feeder);
+
+            ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+            Runnable scrapingTask = () -> {
+                System.out.println("\n=== Nueva ejecución de scraping ===");
+                controller.execute();
+            };
+
+            scheduler.scheduleAtFixedRate(scrapingTask, 0, 1, TimeUnit.HOURS);
+        }
+    }
